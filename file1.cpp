3d59d168fc3271dffe54d08325608dd3b64c083e
Himel777 <himel999777@gmail.com>
2016-12-06 20:30:40 +0600

    edit2 file.cpp

--- file1.cpp
+++ file1.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello Git" << endl;
 	cout << "I am having fun" << endl;
+	cout << "i am having fun" << endl;
+	cout << "its cool to use linux and git" << endl;
 	return 0;
 }
