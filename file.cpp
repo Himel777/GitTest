cc356673d8936eb144dd5197399e3c40b2768b74
Himel777 <himel999777@gmail.com>
2016-12-06 20:05:40 +0600

    created file.cpp

---
+++ file.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+  for(int i=0; i<n; i++) {
+    cout << "iam testing Git" << endl;
+  }
+
+  return 0;
+}
