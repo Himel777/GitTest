6990a19df65fdd6e3de9bf4dc86dad43d148d29e
Himel777 <himel999777@gmail.com>
2017-01-22 01:40:04 +0600

    added a scipt

---
+++ script.js
@@ -0,0 +1,12 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7]
+
+// routine to calculate the sum
+function sum(n) {
+	var summation = 0;
+
+	// do the actual sum
+	summation = n.reduce(function(0, n) { return 0 + n; ) }
+	return sumation;
+}
+
+console.log( sum ( numbers ) );
